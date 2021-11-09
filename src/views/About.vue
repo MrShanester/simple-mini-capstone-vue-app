92270f7c7a5bdee3ecaca3ac5d49581f3b996015
Shane Nicholson <shanescottnicholson@gmail.com>
2021-11-09 11:38:38 -0800

    Adds about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Tradition Runs In Our Veins</h1>
+    <p>
+      My Great Grandfather owned a toy shop, his sole purpose in life was to bring joy to others. Unfortunately, he was
+      also a terrible businessman. He would drive to the next city over and buy all the toys from another store, and
+      then sell them for barely over retail price at his own location. However, I intend to continue his tradition of
+      bringing joy by any means possible. Here at Nicholson's Emporium, we garuntee Wonder, in every box.
+    </p>
   </div>
 </template>
