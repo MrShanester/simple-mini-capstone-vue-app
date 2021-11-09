4dcc2e34991b4ceaec6cf62fb7adaf5bc8a84c85
Shane Nicholson <shanescottnicholson@gmail.com>
2021-11-09 11:18:21 -0800

    Adds get request to localhost:3000 in order to display products in the database

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,42 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <p></p>
+    <div v-for="product in products" v-bind:key="product.id">
+      <h2>{{ product.name }}</h2>
+      <p>Price: ${{ product.price }}</p>
+      <p>--------------------------</p>
+    </div>
   </div>
 </template>
 
-<style></style>
+<style scoped>
+h1 {
+  text-align: center;
+  border: 12px solid;
+  border-image: linear-gradient(135deg, #ff0000 0%, #49ff33 25%, #3364ff 50%, #f6ff33 75%, #ff00a8 100%) 1;
+}
+</style>
 
 <script>
+const axios = require("axios");
+
 export default {
   data: function () {
     return {
-      message: "Welcome to Vue.js!",
+      message: "Welcome To The Shop",
+      products: [],
     };
   },
-  created: function () {},
-  methods: {},
+  created: function () {
+    this.indexProducts();
+  },
+  methods: {
+    indexProducts: function () {
+      axios.get("http://localhost:3000/products").then((response) => {
+        this.products = response.data;
+      });
+    },
+  },
 };
 </script>
